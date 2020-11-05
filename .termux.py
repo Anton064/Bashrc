c07917b4b24a4d19ad867d1f96a8d452144dac8f
Termux32-70 <63185453+Anton064@users.noreply.github.com>
2020-11-05 11:49:32 +0200

    Update and rename bash.bashrc to .termux.py

---
+++ .termux.py
@@ -0,0 +1,34 @@
+import os
+
+os.system("clear")
+
+re ="\033[1;31m"
+wh ="\033[1;37m"
+gr ="\033[1;32m"
+cy ="\033[1;36m"
+sn ="\033[1;35m"
+
+print(f"""
+Welcome to Termux!
+
+Wiki:            {cy}https://wiki.termux.com{wh}
+Community forum: {cy}https://termux.com/community{wh}
+Gitter chat:     {cy}https://gitter.im/termux/termux{wh}
+GitHub:          {cy}https://github.com/Anton064{wh}
+IRC channel:     #termux on freenode
+
+Working with packages:
+
+ * Search packages:   pkg search <query>
+ * Install a package: pkg install <package>
+ * Upgrade packages:  pkg upgrade
+
+Subscribing to additional repositories:
+
+ * Root:     pkg install root-repo
+ * Unstable: pkg install unstable-repo
+ * X11:      pkg install x11-repo
+
+Report issues at {cy}https://termux.com/issues{wh}
+""")
+print("")
